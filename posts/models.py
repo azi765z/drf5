ebec22b89035a97e77cf9f1670ff976fc1f3c41c
aabduraximov2127-art <aabduraximov2127@gmail.com>
2026-07-13 16:04:35 +0500

    frist commit

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class PostModel(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    content = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
