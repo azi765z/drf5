0666a1f7cfa1c56248783f994dd06b1bab79660e
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-07-20 10:09:26 +0500

    funcsion

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,18 @@
+from rest_framework.routers import DefaultRouter
+from . import views
 from django.urls import path
-from .views import CoursesViewSet
 
 urlpatterns = [
-    path('courses/',CoursesViewSet.as_view({'get': 'list','post': 'create',}),),
-    path('cours/<int:pk>/',CoursesViewSet.as_view({'get': 'retrieve','put': 'update','patch': 'partial_update','delete': 'destroy',}),),
+    path('courses/',views.courses_listview),
+    path('courses/create/',views.courses_create),
+    path('courses/<int:pk>/',views.courses_detail),
+    path('courses/<int:pk>/update/',views.courses_update),
+    path('courses/<int:pk>/delete/',views.courses_delete),
+    path('courses/<int:pk>/patch/',views.courses_patch)
+
 ]
+
+
+
+    
+
--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,55 @@
+from itertools import product
+
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import CourseModel
 from .serializer import CourseSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
 
-class CoursesViewSet(viewsets.ModelViewSet):
-    queryset = CourseModel.objects.all()
-    serializer_class = CourseSerializer
+@api_view(['GET'])
+def courses_listview(request):
+    courses = CourseModel.objects.all()
+    serializer = CourseSerializer(courses,many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def courses_create(request):
+    serializer = CourseSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response({"message": "User Create Error"})
+
+@api_view(['GET'])
+def courses_detail(request,pk):
+    courses = CourseModel.objects.get(pk=pk)
+    serializer = CourseSerializer(courses)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def courses_update(request,pk):
+    courses = CourseModel.objects.get(pk=pk)
+    serializer = CourseSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response({'errors'})
+
+
+
+@api_view(['DELETE'])
+def courses_delete(request,pk):
+    courses = CourseModel.objects.get(pk=pk)
+    courses.delete()
+    return Response({'message': 'courses deleted'})
+
+@api_view(['PATCH'])
+def courses_patch(request,pk):
+    courses = CourseModel.objects.get(pk=pk)
+    serializer = CourseSerializer(courses, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response({'errors': serializer.errors})
